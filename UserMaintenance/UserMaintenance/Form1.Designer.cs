4413006a7c073fa877bb21931c9c30e229b634e7
Kelemen Balázs <kelemen.balazs@andoc.net>
2021-11-28 14:38:14 +0100

    Delete function

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -42,13 +42,13 @@ namespace UserMaintenance
             this.lst_Users.FormattingEnabled = true;
             this.lst_Users.Location = new System.Drawing.Point(12, 12);
             this.lst_Users.Name = "lst_Users";
-            this.lst_Users.Size = new System.Drawing.Size(139, 238);
+            this.lst_Users.Size = new System.Drawing.Size(170, 238);
             this.lst_Users.TabIndex = 0;
             // 
             // lbl_LastName
             // 
             this.lbl_LastName.AutoSize = true;
-            this.lbl_LastName.Location = new System.Drawing.Point(157, 24);
+            this.lbl_LastName.Location = new System.Drawing.Point(188, 24);
             this.lbl_LastName.Name = "lbl_LastName";
             this.lbl_LastName.Size = new System.Drawing.Size(35, 13);
             this.lbl_LastName.TabIndex = 1;
@@ -57,34 +57,34 @@ namespace UserMaintenance
             // 
             // txt_LastName
             // 
-            this.txt_LastName.Location = new System.Drawing.Point(227, 21);
+            this.txt_LastName.Location = new System.Drawing.Point(258, 21);
             this.txt_LastName.Name = "txt_LastName";
-            this.txt_LastName.Size = new System.Drawing.Size(181, 20);
+            this.txt_LastName.Size = new System.Drawing.Size(138, 20);
             this.txt_LastName.TabIndex = 3;
             // 
             // btn_Add
             // 
-            this.btn_Add.Location = new System.Drawing.Point(160, 56);
+            this.btn_Add.Location = new System.Drawing.Point(191, 56);
             this.btn_Add.Name = "btn_Add";
-            this.btn_Add.Size = new System.Drawing.Size(121, 23);
+            this.btn_Add.Size = new System.Drawing.Size(100, 23);
             this.btn_Add.TabIndex = 5;
             this.btn_Add.Text = "button1";
             this.btn_Add.UseVisualStyleBackColor = true;
             // 
             // btn_SaveToFile
             // 
-            this.btn_SaveToFile.Location = new System.Drawing.Point(160, 85);
+            this.btn_SaveToFile.Location = new System.Drawing.Point(192, 85);
             this.btn_SaveToFile.Name = "btn_SaveToFile";
-            this.btn_SaveToFile.Size = new System.Drawing.Size(248, 23);
+            this.btn_SaveToFile.Size = new System.Drawing.Size(204, 23);
             this.btn_SaveToFile.TabIndex = 6;
             this.btn_SaveToFile.Text = "button1";
             this.btn_SaveToFile.UseVisualStyleBackColor = true;
             // 
             // btn_Remove
             // 
-            this.btn_Remove.Location = new System.Drawing.Point(287, 56);
+            this.btn_Remove.Location = new System.Drawing.Point(297, 56);
             this.btn_Remove.Name = "btn_Remove";
-            this.btn_Remove.Size = new System.Drawing.Size(121, 23);
+            this.btn_Remove.Size = new System.Drawing.Size(99, 23);
             this.btn_Remove.TabIndex = 7;
             this.btn_Remove.Text = "button1";
             this.btn_Remove.UseVisualStyleBackColor = true;
@@ -93,7 +93,7 @@ namespace UserMaintenance
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(455, 261);
+            this.ClientSize = new System.Drawing.Size(409, 261);
             this.Controls.Add(this.btn_Remove);
             this.Controls.Add(this.btn_SaveToFile);
             this.Controls.Add(this.btn_Add);
